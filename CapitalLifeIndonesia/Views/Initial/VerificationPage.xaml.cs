98ea9699abb2154f23138e34ca4d84c9565a1280
Mufti Alie S <iyakiwan19@gmail.com>
2021-08-10 17:59:48 +0700

    Proggress 10/08/2021

--- CapitalLifeIndonesia/Views/Initial/VerificationPage.xaml.cs
+++ CapitalLifeIndonesia/Views/Initial/VerificationPage.xaml.cs
@@ -29,7 +29,6 @@ namespace CapitalLifeIndonesia.Views.Initial
 
         private void LabelResend_Tapped(object sender, EventArgs e)
         {
-            //Navigation.RemovePage(Navigation.NavigationStack[Navigation.NavigationStack.Count - 2]);
             var input1 = tvNumber1.Text;
             var input2 = tvNumber2.Text;
             var input3 = tvNumber3.Text;
@@ -38,6 +37,10 @@ namespace CapitalLifeIndonesia.Views.Initial
             if (string.IsNullOrEmpty(input1) || string.IsNullOrEmpty(input2) || string.IsNullOrEmpty(input3) || string.IsNullOrEmpty(input4))
             {
                 DependencyService.Get<IMessage>().ShortAlert("Verifikasi Token");
+            }
+            else if (!input1.Equals("1") || !input2.Equals("2") || !input3.Equals("3") || !input4.Equals("4"))
+            {
+                navFail();
             }
             else
             {
@@ -51,10 +54,15 @@ namespace CapitalLifeIndonesia.Views.Initial
             await Navigation.PushAsync(new MainPage());
         }
 
+        private async void navFail()
+        {
+            await Navigation.PushPopupAsync(new VerifyFailPage());
+        }
+
         private async void LabelFailed_Tapped(object sender, EventArgs e)
         {
             //Navigation.RemovePage(Navigation.NavigationStack[Navigation.NavigationStack.Count - 2]);
-            await Navigation.PushPopupAsync(new VerifyFailPage());
+            navFail();
         }
     }
 }
